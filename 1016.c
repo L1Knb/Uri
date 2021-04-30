e02b0d7969250f288321b56edd756d73a21d94f3
Eliú Kneib <44679976+L1Knb@users.noreply.github.com>
2021-04-30 14:16:31 -0300

    Add files via upload

---
+++ 1016.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    int dif;
+    scanf("%d",&dif);
+    printf("%d minutos\n",2*dif);
+    
+    return 0;
+}
---
+++ 1017.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int t,v;
+    float g;
+     scanf("%d %d", &t, &v);
+     g = ((t*v) / 12.0);
+     printf("%.3f\n", g);
+    
+    return 0;
+}
